57ea45b13c2c86ab39c3697c63d3f8ef6beacdf0
Christian Dickreuter <christian@dickreuter-digital.de>
2020-10-07 17:02:36 +0200

    Auth und Restaurant Controller angelegt

---
+++ lieblings_restaurants_api/app/Http/Controllers/AuthController.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class AuthController extends Controller
+{
+    public function login(Request $request)
+    {
+        $loginData = $request->validate([
+            'email' => 'email|required',
+            'password' => 'required'
+        ]);
+
+        if(!auth()->attempt($loginData)) {
+            return response(['message' => 'E-Mail oder Passwort falsch!']);
+        }
+
+        $accessToken = auth()->user()->createToken('authToken')->accessToken;
+
+        return response(['user' => auth()->user(), 'access_token' => $accessToken]);
+    }
+}
