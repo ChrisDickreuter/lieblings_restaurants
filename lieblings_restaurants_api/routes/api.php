528152384b5df13a1def08118c61a571a0afa07e
Christian Dickreuter <christian@dickreuter-digital.de>
2020-10-07 17:03:09 +0200

    Routes angelegt

--- lieblings_restaurants_api/routes/api.php
+++ lieblings_restaurants_api/routes/api.php
@@ -13,7 +13,12 @@ use Illuminate\Support\Facades\Route;
 | is assigned the "api" middleware group. Enjoy building your API!
 |
 */
+Route::post('/login', 'AuthController@login');
 
-Route::middleware('auth:api')->get('/user', function (Request $request) {
-    return $request->user();
-});
+Route::get('/login', function () {
+    return 'Access token is wrong or missing';
+})->name('login');
+
+Route::apiResource('/restaurants', 'RestaurantController')->middleware('auth:api'); 
+
+
