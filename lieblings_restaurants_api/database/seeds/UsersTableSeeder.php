f73b739ac57a441eb68ad9ac52fd2e4b7953497f
Christian Dickreuter <christian@dickreuter-digital.de>
2020-12-08 17:38:17 +0100

    User und Restaurants Seeder

--- lieblings_restaurants_api/database/seeds/UsersTableSeeder.php
+++ lieblings_restaurants_api/database/seeds/UsersTableSeeder.php
@@ -11,10 +11,27 @@ class UsersTableSeeder extends Seeder
      */
     public function run()
     {
-        DB::table('users')->insert([
-            'name' => 'Christian',
-            'email' => 'christian@dickreuter-digital.de',
-            'password' => Hash::make('passwort'),
+        DB::table('users')->insert([ 
+            [
+                'name' => 'Chris',
+                'email' => 'christian@dickreuter-digital.de',
+                'password' => Hash::make('bolo')
+            ],
+            [
+                'name' => 'Jenny',
+                'email' => 'jenny@dickreuter-digital.de',
+                'password' => Hash::make('panda')
+            ],
+            [
+                'name' => 'Harry',
+                'email' => 'harry@dickreuter-digital.de',
+                'password' => Hash::make('speck')
+            ],
+            [
+                'name' => 'Beate',
+                'email' => 'beate@dickreuter-digital.de',
+                'password' => Hash::make('bolo')
+            ],
         ]);
     }
 }
