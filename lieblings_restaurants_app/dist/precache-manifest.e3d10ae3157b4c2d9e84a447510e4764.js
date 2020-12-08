d78fdd19114bab9dfba5c8a1e235427dd90d12c3
Christian Dickreuter <christian@dickreuter-digital.de>
2020-12-08 17:56:13 +0100

    Dist Verzeichnis

---
+++ lieblings_restaurants_app/dist/precache-manifest.e3d10ae3157b4c2d9e84a447510e4764.js
@@ -0,0 +1,74 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0c5c1b7991e723f68f22",
+    "url": "/css/about~items.dd6c52f9.css"
+  },
+  {
+    "revision": "1c7a08d57a201325dc00",
+    "url": "/css/chunk-vendors.e27e4e4b.css"
+  },
+  {
+    "revision": "4f90e8ea32835f4a9ad6",
+    "url": "/css/items.c8467209.css"
+  },
+  {
+    "revision": "3c7edb522897b8e1fc1e",
+    "url": "/css/login.4044c755.css"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/fonts/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/fonts/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/fonts/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/fonts/fontawesome-webfont.fee66e71.woff"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/img/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "575edd78a0046ef068c0e1a8ea02db62",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c10cb5e20b1b8e9b3b61",
+    "url": "/js/about.f724a642.js"
+  },
+  {
+    "revision": "0c5c1b7991e723f68f22",
+    "url": "/js/about~items.10cd06cf.js"
+  },
+  {
+    "revision": "3903b16bdd2773fd069d",
+    "url": "/js/app.d41285d9.js"
+  },
+  {
+    "revision": "1c7a08d57a201325dc00",
+    "url": "/js/chunk-vendors.8a2dc8f3.js"
+  },
+  {
+    "revision": "4f90e8ea32835f4a9ad6",
+    "url": "/js/items.8059db0a.js"
+  },
+  {
+    "revision": "3c7edb522897b8e1fc1e",
+    "url": "/js/login.88cfd79f.js"
+  },
+  {
+    "revision": "66d8730c0728d155d4af410e05d8030d",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "735ab4f94fbcd57074377afca324c813",
+    "url": "/robots.txt"
+  }
+]);
